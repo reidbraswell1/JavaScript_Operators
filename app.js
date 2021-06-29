dee15dd0505a8f45200e8e62dd8cc1139eac21ba
reid <reidbras@gmail.com>
2021-06-28 21:12:32 -0500

    Initial revision

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+console.log("EXERCISE 1:\n==========\n");
+
+// YOUR CODE HERE
+
+// Exercise 2
+console.log("EXERCISE 2:\n==========\n");
+
+// YOUR CODE HERE
+
+// Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+
+// YOUR CODE HERE
