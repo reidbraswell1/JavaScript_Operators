c6438da44cf20a174f9aacdfc31381dcf21f0faf
reid <reidbras@gmail.com>
2021-06-29 21:27:52 -0500

    updating exercise code

--- app.js
+++ app.js
@@ -15,33 +15,39 @@ You can print the value of the variables to the browser console (ex: `console.lo
 
 */
 // YOUR CODE HERE
-let a = 20;
-let b = 4;
-let add = a + b;
-let minus = a - b;
-let multiply = a * b;
-let dividing = a / b;
-console.log("EXERCISE 1:\n==========\n");
+{
+  let a = 20;
+  let b = 4;
+  let add = a + b;
+  let minus = a - b;
+  let multiply = a * b;
+  let dividing = a / b;
+  console.log("EXERCISE 1:\n==========\n");
 
-console.log(`add = ${add}`);
-let node = document.createElement("li");
-node.innerText = `add = ${add}`;
-document.getElementById("exercise-1").appendChild(node);
+  let node = document.createElement("li");
+  node.innerText = `Let a = ${a}, b = ${b}`;
+  document.getElementById("exercise-1").appendChild(node);
 
-console.log(`minus = ${minus}`);
-node = document.createElement("li");
-node.innerText = `minus = ${minus}`;
-document.getElementById("exercise-1").appendChild(node);
+  console.log(`add (a + b) = ${add}`);
+  node = document.createElement("li");
+  node.innerText = `add (a + b) = ${add}`;
+  document.getElementById("exercise-1").appendChild(node);
 
-console.log(`multiply = ${multiply}`);
-node = document.createElement("li");
-node.innerText = `multiply = ${multiply}`;
-document.getElementById("exercise-1").appendChild(node);
+  console.log(`minus (a - b) = ${minus}`);
+  node = document.createElement("li");
+  node.innerText = `minus (a - b) = ${minus}`;
+  document.getElementById("exercise-1").appendChild(node);
 
-console.log(`dividing = ${dividing}`);
-node = document.createElement("li");
-node.innerText = `dividing = ${dividing}`;
-document.getElementById("exercise-1").appendChild(node);
+  console.log(`multiply (a * b) = ${multiply}`);
+  node = document.createElement("li");
+  node.innerText = `multiply (a * b) = ${multiply}`;
+  document.getElementById("exercise-1").appendChild(node);
+
+  console.log(`dividing (a / b) = ${dividing}`);
+  node = document.createElement("li");
+  node.innerText = `dividing (a / b) = ${dividing}`;
+  document.getElementById("exercise-1").appendChild(node);
+}
 // Exercise 2
 /*
 1. Use the following code to answer the questions below:
@@ -65,38 +71,45 @@ Use the code above to test and print the results.
 console.log("EXERCISE 2:\n==========\n");
 */
 // YOUR CODE HERE
-let c = 11;
-let str = "11";
-let str2 = "eleven";
-let isPresent = true;
-let firstName = "Jackie";
-let lastName = "Chan";
-console.log("EXERCISE 2:\n==========\n");
-console.log(`c + str = ${c + str}`);
-node = document.createElement("li");
-node.innerText = `c + str = ${c + str}`;
-document.getElementById("exercise-2").appendChild(node);
+{
+  let a = 11;
+  let str = "11";
+  let str2 = "eleven";
+  let isPresent = true;
+  let firstName = "Jackie";
+  let lastName = "Chan";
 
-console.log(`c + str2 = ${c + str2}`);
-node = document.createElement("li");
-node.innerText = `c + str2 = ${c + str2}`;
-document.getElementById("exercise-2").appendChild(node);
+  console.log("EXERCISE 2:\n==========\n");
 
-console.log(`c + isPresent = ${c + isPresent}`);
-node = document.createElement("li");
-node.innerText = `c + isPresent = ${c + isPresent}`;
-document.getElementById("exercise-2").appendChild(node);
+  let node = document.createElement("li");
+  node.innerText = `Let a = ${a}, str = "${str}", str2 = ${str2}, isPresent = ${isPresent}, firstName = ${firstName}, lastName = ${lastName}`;
+  document.getElementById("exercise-2").appendChild(node);
 
-console.log(`c + firstName = ${c + firstName}`);
-node = document.createElement("li");
-node.innerText = `c + firstName = ${c + firstName}`;
-document.getElementById("exercise-2").appendChild(node);
+  console.log(`(a + str) = ${a + str}`);
+  node = document.createElement("li");
+  node.innerText = `(a + str) = ${a + str}`;
+  document.getElementById("exercise-2").appendChild(node);
 
-console.log(`c + lastName = ${c + lastName}`);
-node = document.createElement("li");
-node.innerText = `c + lastName = ${c + lastName}`;
-document.getElementById("exercise-2").appendChild(node);
+  console.log(`(a + str2) = ${a + str2}`);
+  node = document.createElement("li");
+  node.innerText = `(a + str2) = ${a + str2}`;
+  document.getElementById("exercise-2").appendChild(node);
 
+  console.log(`(a + isPresent) = ${a + isPresent}`);
+  node = document.createElement("li");
+  node.innerText = `(a + isPresent) = ${a + isPresent}`;
+  document.getElementById("exercise-2").appendChild(node);
+
+  console.log(`(a + firstName) = ${a + firstName}`);
+  node = document.createElement("li");
+  node.innerText = `(a + firstName) = ${a + firstName}`;
+  document.getElementById("exercise-2").appendChild(node);
+
+  console.log(`(a + lastName) = ${a + lastName}`);
+  node = document.createElement("li");
+  node.innerText = `(a + lastName) = ${a + lastName}`;
+  document.getElementById("exercise-2").appendChild(node);
+}
 // Exercise 3
 /*
 let a = 5;
@@ -118,42 +131,63 @@ let lastName = "Williams";
 
 Use the code above to test and print the results.
 */
-let d = 5;
-let str3 = "5";
-let str4 = "five";
-let isPresent2 = false;
-let firstName2 = "Robin";
-let lastName2 = "Williams";
-
-console.log("EXERCISE 3:\n==========\n");
-console.log(`d == str3 = ${d == str3}`);
-node = document.createElement("li");
-node.innerText = `d == str3 = ${d == str3}`;
-document.getElementById("exercise-3").appendChild(node);
-
-console.log(`d === str3 = ${d === str3}`);
-node = document.createElement("li");
-node.innerText = `d === str3 = ${d === str3}`;
-document.getElementById("exercise-3").appendChild(node);
-
-console.log(`!isPresent2 = ${!isPresent2}`);
-node = document.createElement("li");
-node.innerText = `!isPresent2 = ${!isPresent2}`;
-document.getElementById("exercise-3").appendChild(node);
-
-console.log(`eleven == str4 && d >= str3 = ${"eleven" == str4 && d >= str3}`);
-node = document.createElement("li");
-node.innerText = `eleven == str4 && d >= str3 = ${"eleven" == str4 && d >= str3}`;
-document.getElementById("exercise-3").appendChild(node);
-
-console.log(`!isPresent || isPresent = ${!isPresent || isPresent}`);
-node = document.createElement("li");
-node.innerText = `!isPresent || isPresent = ${!isPresent || isPresent}`;
-document.getElementById("exercise-3").appendChild(node);
-
-console.log(`0 == false = ${0 == false}`);
-node = document.createElement("li");
-node.innerText = `0 == false = ${0 == false}`;
-document.getElementById("exercise-3").appendChild(node);
-
 // YOUR CODE HERE
+{
+  let a = 5;
+  let str = "5";
+  let str2 = "five";
+  let isPresent = false;
+  let firstName = "Robin";
+  let lastName = "Williams";
+
+  console.log("EXERCISE 3:\n==========\n");
+
+  let node = document.createElement("li");
+  node.innerText = `Let a = ${a}, str = "${str}", str2 = ${str2}, isPresent = ${isPresent}, firstName = ${firstName}, lastName = ${lastName}`
+  document.getElementById("exercise-3").appendChild(node);
+
+  console.log(`(a == str) = ${a == str}`);
+  node = document.createElement("li");
+  node.innerText = `(a == str) = ${a == str}`;
+  document.getElementById("exercise-3").appendChild(node);
+
+  console.log(`(a === str) = ${a === str}`);
+  node = document.createElement("li");
+  node.innerText = `(a === str) = ${a === str}`;
+  document.getElementById("exercise-3").appendChild(node);
+
+  console.log(`(!isPresent) = ${!isPresent}`);
+  node = document.createElement("li");
+  node.innerText = `(!isPresent) = ${!isPresent}`;
+  document.getElementById("exercise-3").appendChild(node);
+
+  console.log(`(eleven == str2 && a >= str) = ${"eleven" == str2 && a >= str}`);
+  node = document.createElement("li");
+  node.innerText = `(eleven == str2 && a >= str) = ${"eleven" == str2 && a >= str}`;
+  document.getElementById("exercise-3").appendChild(node);
+
+  console.log(`(!isPresent || isPresent) = ${!isPresent || isPresent}`);
+  node = document.createElement("li");
+  node.innerText = `(!isPresent || isPresent) = ${!isPresent || isPresent}`;
+  document.getElementById("exercise-3").appendChild(node);
+
+  console.log(`(0 == false) = ${0 == false}`);
+  node = document.createElement("li");
+  node.innerText = `(0 == false) = ${0 == false}`;
+  document.getElementById("exercise-3").appendChild(node);
+
+  console.log(`(0 === false) = ${0 === false}`);
+  node = document.createElement("li");
+  node.innerText = `(0 === false) = ${0 === false}`;
+  document.getElementById("exercise-3").appendChild(node);
+
+- console.log(`(0 != false) = ${0 != false}`);
+  node = document.createElement("li");
+  node.innerText = `(0 != false) = ${0 != false}` 
+  document.getElementById("exercise-3").appendChild(node);
+
+- console.log(`(0 !== false) = ${0 !== false}`);
+  node = document.createElement("li");
+  node.innerText = `(0 !== false) = ${0 !== false}`;
+  document.getElementById("exercise-3").appendChild(node);
+}
